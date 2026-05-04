e92ac9ee3c1e6bb2f3b4f6030ea7752c62878c4a
financeaksata26 <financeaksata26@gmail.com>
2026-05-04 10:42:09 +0700

    Update config.js

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -2,7 +2,7 @@
 // config.js – Ganti GAS_URL setelah deploy Apps Script
 // ============================================================
 
-const GAS_URL = 'https://script.google.com/macros/s/PASTE_YOUR_DEPLOYMENT_ID_HERE/exec';
+const GAS_URL = 'https://script.google.com/macros/s/AKfycbwyrbYKNVUMHiQEsT6trVLfrokFjDUg6bQ2aPKqFsVyI34j3AL8_hO5cB5Ifg5Om79H/exec';
 
 // Versi & info aplikasi
 const APP_VERSION = '1.0.0';
